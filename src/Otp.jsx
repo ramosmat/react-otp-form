66e308bab1e90591e24cd0af7d5aa89ee205795f
Matheus <matheusramosbi.13@gmail.com>
2024-07-30 13:58:22 -0300

    handleChange function

--- src/Otp.jsx
+++ src/Otp.jsx
@@ -2,19 +2,89 @@ import React from 'react';
 import styles from './Otp.module.css';
 
 const Otp = () => {
+  const ref = React.useRef(null);
+  const [otp, setOtp] = React.useState(['', '', '', '', '']);
+
+  React.useEffect(() => {
+    ref.current?.focus();
+  }, []);
+
   function handleSubmit(event) {
     event.preventDefault();
+  }
+
+  function handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    const nextInput = event.target.nextElementSibling;
+
+    const index = name.split('-')[1];
+
+    setOtp((prevOtp) => {
+      const newOTP = [...prevOtp];
+      newOTP[index] = value;
+      return newOTP;
+    });
+
+    if (value && nextInput) {
+      nextInput.focus();
+    }
   }
 
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
       <div className={styles.divInput}>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+        <input
+          inputMode="numeric"
+          autoComplete="one-time-code"
+          name="otp-0"
+          required
+          maxLength={1}
+          type="text"
+          value={otp[0]}
+          onChange={handleChange}
+          ref={ref}
+        />
+        <input
+          inputMode="numeric"
+          autoComplete="one-time-code"
+          name="otp-1"
+          required
+          maxLength={1}
+          type="text"
+          value={otp[1]}
+          onChange={handleChange}
+        />
+        <input
+          inputMode="numeric"
+          autoComplete="one-time-code"
+          name="otp-2"
+          required
+          maxLength={1}
+          type="text"
+          value={otp[2]}
+          onChange={handleChange}
+        />
+        <input
+          inputMode="numeric"
+          autoComplete="one-time-code"
+          name="otp-3"
+          required
+          maxLength={1}
+          type="text"
+          value={otp[3]}
+          onChange={handleChange}
+        />
+        <input
+          inputMode="numeric"
+          autoComplete="one-time-code"
+          name="otp-4"
+          required
+          maxLength={1}
+          type="text"
+          value={otp[4]}
+          onChange={handleChange}
+        />
       </div>
       <button>Verificar OTP</button>
     </form>
